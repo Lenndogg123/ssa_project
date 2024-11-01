974e4cfb8f448b96b163dfc77b2472cd3baceb97
Lenndogg123 <lboemi@parrastu.catholic.edu.au>
2024-11-01 13:44:22 +1100

    week 2 errors

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpReponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required

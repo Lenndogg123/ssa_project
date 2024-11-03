5fe1c61f85f64c6d2efec4eab47d2bee7aeb2b5a
Lenndogg123 <lboemi@parrastu.catholic.edu.au>
2024-11-03 17:09:25 +1100

    working on fixing error for AttributeError: module 'chipin.views' has no attribute 'home'

--- ssa_project/settings.py
+++ ssa_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!x8vbcbjhy8pd0cx=ger=$tn!)(kqhz4lx$0!%l3=b$22tsdx6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages 
 from .forms import UserRegistrationForm
 
 def register(request):
